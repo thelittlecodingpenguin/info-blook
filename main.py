487cc58e18f7544ece4248e1074166073a9a151c
thelittlecodingpenguin <68244236+thelittlecodingpenguin@users.noreply.github.com>
2022-04-03 09:58:27 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+wonderland = {"two of spades": ["15.2%", "Wonderland", "Uncommon", round((100/15.2))*15], "alice": ["15%", "Wonderland", "Uncommon", round((100/15))*15], "queen of hearts": ["15%", "Wonderland", "Uncommon", round((100/15))*15], "eat me": ["15%", "Wonderland", "Uncommon", round((100/15))*15], "drink me": ["15%", "Wonderland", "Uncommon", round((100/15))*15], "dormouse": ["6.5%", "Wonderland", "Rare", round((100/6.5))*15], "ceshire cat": ["6.5%", "Wonderland", "Rare", round((100/6.5))*15], "white rabbit": ["6.5%", "Wonderland", "Rare", round((100/6.5))*15], "caterpillar": ["2.5%", "Wonderland", "Epic", round((100/2.5))*15], "mad hatter": ["2.5%", "Wonderland", "Epic", round((100/2.5))*15], "king of hearts": ["0.3%", "Wonderland", "Legendary", round((100/0.3))*15]}
+
+breakfast = {"orange juice": ["12.5%", "Breakfast", "Uncommon", round((100/12.5))*15], "toast": ["12.5%", "Breakfast", "Uncommon", round((100/12.5))*15], "cereal": ["12.5%", "Breakfast", "Uncommon", round((100/12.5))*15], "breakfast combo": ["12.5%", "Breakfast", "Uncommon", round((100/12.5))*15], "yogurt": ["12.5%", "Breakfast", "Uncommon", round((100/12.5))*15], "milk": ["12.5%", "Breakfast", "Uncommon", round((100/12.5))*15], "waffle": ["9%", "Breakfast", "Rare", round((100/9))*15], "pancakes": ["9%", "Breakfast", "Rare", round((100/9))*15], "french toast": ["5%", "Breakfast", "Epic", round((100/5))*15], "pizza": ["2%", "Breakfast", "Epic", round((100/2))*15]}
+
+medieval = {"witch": ["13.4%", "Medieval", "Uncommon", round((100/13.4))*15], "wizard": ["13.4%", "Medieval", "Uncommon", round((100/13.4))*15], "slime monster": ["13.4%", "Medieval", "Uncommon", round((100/13.4))*15], "elf": ["13.4%", "Medieval", "Uncommon", round((100/13.4))*15], "fairy": ["13.4%", "Medieval", "Uncommon", round((100/13.4))*15], "dragon": ["9%", "Medieval", "Rare", round((100/9))*15], "jester": ["9%", "Medieval", "Rare", round((100/9))*15], "queen": ["9%", "Medieval", "Rare", round((100/9))*15], "unicorn": ["5%", "Medieval", "Epic", round((100/5))*15], "king": ["1%", "Medieval", "Legendary", round((100/1))*15]}
+
+space = {"alien": ["18.75%", "Space", "Uncommon", round((100/18.75))*20], "earth": ["18.75%", "Space", "Uncommon", round((100/18.75))*20], "meteor": ["18.75%", "Space", "Uncommon", round((100/18.75))*20], "stars": ["18.75%", "Space", "Uncommon", round((100/18.75))*20], "planet": ["10%", "Space", "Rare", round((100/10))*20], "ufo": ["10%", "Space", "Rare", round((100/10))*20], "spaceship": ["4.5%", "Space", "Epic", round((100/4.5))*20], "astronaut": ["0.45%", "Space", "Legendary", round((100/0.45))*20], "colored astronaut": ["0.05%", "Space", "Chroma", round((100/0.05))*20]}
+
+bot = {"lil bot": ["19.5%", "Bot", "Uncommon", round((100/19.5))*20], "lovely bot": ["19.5%", "Bot", "Uncommon", round((100/19.5))*20], "angry bot": ["19.5%", "Bot", "Uncommon", round((100/19.5))*20], "happy bot": ["19.5%", "Bot", "Uncommon", round((100/19.5))*20], "watson": ["9%", "Bot", "Rare", round((100/9))*20], "buddy bot": ["9%", "Bot", "Rare", round((100/9))*20], "brainy bot": ["3.7%", "Bot", "Epic", round((100/3.7))*20], "mega bot": ["0.3%", "Bot", "Legendary", round((100/0.3))*20]}
+
+aquatic = {"jellyfish": ["15%", "Aquatic", "Uncommon", round((100/15))*20], "old boot": ["15%", "Aquatic", "Uncommon", round((100/15))*20], "frog": ["15%", "Aquatic", "Uncommon", round((100/15))*20], "clownfish": ["15%", "Aquatic", "Uncommon", round((100/15))*20], "crab": ["15%", "Aquatic", "Uncommon", round((100/15))*20], "pufferfish": ["6.8%", "Aquatic", "Rare", round((100/6.8))*20], "blobfish": ["6.8%", "Aquatic", "Rare", round((100/6.8))*20], "octopus": ["6.8%", "Aquatic", "Rare", round((100/6.8))*20], "narwhal": ["3.9%", "Aquatic", "Epic", round((100/3.9))*20], "baby shark": ["0.5%", "Aquatic", "Legendary", round((100/0.5))*20], "megaladon": ["0.2%", "Aquatic", "Legendary", round((100/0.2))*20]}
+
+safari = {"panda": ["15%", "Safari", "Uncommon", round((100/15))*25], "tenrec": ["15%", "Safari", "Uncommon", round((100/15))*25], "sloth": ["15%", "Safari", "Uncommon", round((100/15))*25], "zebra": ["15%", "Safari", "Uncommon", round((100/15))*25], "flamingo": ["15%", "Safari", "Uncommon", round((100/15))*25], "pecock": ["7%", "Safari", "Rare", round((100/7))*25], "elephant": ["7%", "Safari", "Rare", round((100/7))*25], "lemur": ["7%", "Safari", "Rare", round((100/7))*25], "chameleon": ["3.48%", "Safari", "Epic", round((100/3.48))*25], "lion": ["0.5%", "Safari", "Legendary", round((100/0.5))*25], "rainbow panda": ["0.02%", "Safari", "Chroma", round((100/0.02))*25]}
+
+blizzard = {"snow globe": ["17.75%", "Blizzard", "Uncommon", round(100/17.75)*25], "holiday gift": ["17.75%", "Blizzard", "Uncommon", round(100/17.75)*25], "hot chocolate": ["17.75%", "Blizzard", "Uncommon", round(100/17.75)*25], "holiday wreath": ["17.75%", "Blizzard", "Uncommon", round(100/17.75)*25], "gingerbread man": ["11.45%", "Blizzard", "Rare", round(100/11.45)*25], "gingerbread house": ["11.45%", "Blizzard", "Rare", round(100/11.45)*25], "snowman": ["5.05%", "Blizzard", "Epic", round(100/5.05)*25], "santa claus": ["1%", "Blizzard", "Legendary", round(100/1)*25], "frost wreath": ["0.03%", "Blizzard", "Chroma", round(100/0.03)*25], "tropical globe": ["0.02%", "Blizzard", "Chroma", round(100/0.02)*25]}
+
+spooky = {"pumpkin": ["18.5%", "Spooky", "Uncommon", round(100/18.5)*25], "swamp monster": ["18.5%", "Spooky", "Uncommon", round(100/18.5)*25], "vampire": ["18.5%", "Spooky", "Uncommon", round(100/18.5)*25], "frankenstein": ["18.5%", "Spooky", "Uncommon", round(100/18.5)*25], "zombie": ["10.15%", "Spooky", "Rare", round(100/10.15)*25], "mummy": ["10.15%", "Spooky", "Rare", round(100/10.15)*25], "werewolf": ["5%", "Spooky", "Epic", round(100/5)*25], "ghost": ["0.65%", "Spooky", "Legendary", round(100/0.65)*25], "haunted pumpkin": ["0.05%", "Spooky", "Chroma", round(100/0.05)*25]}
+
+easter = {"blue egg": ["14.49%", "Easter", "Uncommon", round(100/14.49)*25], "purple egg": ["14.49%", "Easter", "Uncommon", round(100/14.49)*25], "orange egg": ["14.49%", "Easter", "Uncommon", round(100/14.49)*25], "red egg": ["14.49%", "Easter", "Uncommon", round(100/14.49)*25], "pink egg": ["14.49%", "Easter", "Uncommon", round(100/14.49)*25], "yellow egg": ["6%", "Easter", "Rare", round(100/6)*25], "black egg": ["6%", "Easter", "Rare", round(100/6)*25], "lime egg": ["6%", "Easter", "Rare", round(100/6)*25], "chocolate": ["6%", "Easter", "Rare", round(100/6)*25], "easter basket": ["3%", "Easter", "Epic", round(100/3)*25], "easter bunny": ["0.5%", "Easter", "Legendary", round(100/0.5)*25], "diamond egg": ["0.05%", "Easter", "Chroma", round(100/0.05)*25]}
+
+# print(wonderland["king of hearts"])
+# print(breakfast["pizza"])
+# print(medieval["king"])
+# print(space["colored astronaut"])
+# print(bot["mega bot"])
+# print(aquatic["megaladon"])
+# print(safari["rainbow panda"])
+
+boxes = [safari, bot, space, aquatic, breakfast, medieval, wonderland, blizzard, spooky, easter]
+print("For colored astronaut, they are all the same rarity and description, so if you want to see it, type colored astronaut. ")
+blook = input("What Blook do you want to see? \n >>> ")
+blook = blook.lower()
+
+phrases = ["chance", "box", "", "tokens on average to get it"]
+k = 0
+
+for box in boxes:
+    try:
+        #TODO: Convert 4th value to str
+        
+        for desc in box[blook]:
+            print(desc, phrases[k])
+            k+=1
+        break
+    except KeyError:
+        continue
+
+    print("Blook not found")
